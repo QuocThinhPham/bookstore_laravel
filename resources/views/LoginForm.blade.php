cea6b5c7417199828ec4acd300f31763f9c801de
= <=>
2019-11-23 09:00:32 +0700

    config psw input

--- resources/views/LoginForm.blade.php
+++ resources/views/LoginForm.blade.php
@@ -14,7 +14,7 @@
     <div class="form-group">
       <label class="control-label col-lg-4">Password</label>
       <div class="col-lg-8">
-        <input type="number" name="phone-number" id="" class="form-control">
+        <input type="password" name="pass" id="" class="form-control">
       </div>
     </div>
     <div class="form-group text-center">
